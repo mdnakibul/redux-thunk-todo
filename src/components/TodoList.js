e3e699272617e526273454973c075c26c0fd95a1
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2025-05-29 08:05:51 +0600

    add fetchTodos thunk and useDispatch to TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
-import { useSelector } from "react-redux";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux";
 import Todo from "./Todo";
+import fetchTodos from '../redux/thunk/fetchTodos';
 
 export default function TodoList() {
     const todos = useSelector(state => state.todos)
     const filters = useSelector(state => state.filters)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchTodos)
+    }, [dispatch])
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
             {
