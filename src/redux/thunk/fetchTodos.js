c89dff2504746cb23572b80cb4adf6e6cd985ab9
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2025-05-29 08:06:07 +0600

    add fetchTodos thunk to retrieve todos from the server

---
+++ src/redux/thunk/fetchTodos.js
@@ -0,0 +1,10 @@
+import { todoLoaded } from "../todo/actions";
+
+const fetchTodos = async (dispatch) => {
+    const response = await fetch("http://localhost:9000/todos");
+    const todos = await response.json();
+
+    dispatch(todoLoaded(todos));
+};
+
+export default fetchTodos;
